242339d3a757f2d18e4a068ca478869d672a35e8
syedly <hassanshah9153@gmail.com>
2025-10-21 14:22:10 +0100

    added user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None  # disable username
+    email = models.EmailField(unique=True)
+
+    gender = models.CharField(max_length=50, null=True, blank=True)
+    date_of_birth = models.DateTimeField(null=True, blank=True)
+    religion = models.CharField(max_length=50, null=True, blank=True)
+    status = models.CharField(max_length=50, null=True, blank=True)
+    mobile_no = models.CharField(max_length=50, null=True, blank=True)
+    profile_createdby = models.IntegerField(null=True, blank=True)
+    marital_status = models.CharField(max_length=50, null=True, blank=True)
+    cast = models.CharField(max_length=50, null=True, blank=True)
+    height = models.CharField(max_length=50, null=True, blank=True)
+    education = models.CharField(max_length=50, null=True, blank=True)
+    city = models.IntegerField(null=True, blank=True)
+    occupation = models.CharField(max_length=50, null=True, blank=True)
+    terms = models.BooleanField(null=True, blank=True)
+    role_id = models.IntegerField(null=True, blank=True)
+    user_image = models.TextField(null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    userKaTaruf = models.TextField(null=True, blank=True)
+    userDiWohtiKaTaruf = models.TextField(null=True, blank=True)
+    call_status = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True, blank=True)
+    subscribtionStatus = models.CharField(max_length=50, null=True, blank=True)
+    subscribtionExpire = models.DateTimeField(null=True, blank=True)
+    subscribtionDate = models.CharField(max_length=50, null=True, blank=True)
+    connections = models.IntegerField(null=True, blank=True)
+    connectionsDescription = models.TextField(null=True, blank=True)
+    tiktokLink = models.TextField(null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # email and password are required by default
+
+    objects = CustomUserManager()
+
+    class Meta:
+        db_table = 'Users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.email
--- asanrishta/settings.py
+++ asanrishta/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -72,10 +75,21 @@ WSGI_APPLICATION = 'asanrishta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '3306',
     }
 }
 
@@ -120,3 +134,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'app.CustomUser'
